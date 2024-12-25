fe12c8c97ec25b72d899ebbd0ce253863c487fa5
codersantosh <codersantosh@gmail.com>
2024-12-25 20:14:21 +0545

    Added default menu on navigation pattern

--- patterns/navigation-menu.php
+++ patterns/navigation-menu.php
@@ -10,4 +10,14 @@
  */
 
 ?>
-<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"},"style":{"typography":{"textTransform":"uppercase","fontSize":"12px","letterSpacing":"1px","fontStyle":"normal","fontWeight":"700"}}} /-->
+<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"},"style":{"typography":{"textTransform":"uppercase","fontSize":"12px","letterSpacing":"1px","fontStyle":"normal","fontWeight":"700"}}} -->
+	<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Home', 'patterns-agency' ); ?>","url":"#"} /-->
+
+	<!-- wp:navigation-link {"label":"<?php esc_html_e( 'About', 'patterns-agency' ); ?>","url":"#"} /-->
+
+	<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Services', 'patterns-agency' ); ?>","url":"#"} /-->
+
+	<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Blog', 'patterns-agency' ); ?>","url":"#"} /-->
+
+	<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Contact', 'patterns-agency' ); ?>","url":"#"} /-->
+<!-- /wp:navigation -->
